6114fd1c52fa8aa3764f1284ff381c00c304c831
mathi1322 <mathivanan@drylogics.com>
2021-05-12 18:15:51 +0530

    Renamed

---
+++ is_prime?.rb
@@ -0,0 +1,7 @@
+# finds if a number is odd or even
+def is_prime?(n)      
+    if n % 2 == 0
+        return true
+    else return false
+    end 
+end
--- odd_or_even.rb
+++
@@ -1,9 +0,0 @@
-def evenodd(n)      
-    if n % 2 == 0
-        return "#{n} is even"
-    else return "#{n} is odd"
-    end 
-end
-print "Enter n = "
-n = gets.chomp.to_i 
-puts evenodd(n)
