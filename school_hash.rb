779573660d7673efe7ce290a64b10766b039c166
mathi1322 <mathivanan@drylogics.com>
2021-06-07 14:02:25 +0530

    Changed into simple codes

--- school_hash.rb
+++ school_hash.rb
@@ -14,62 +14,40 @@ school_hash = [
 ]
 puts "1) Has teacher Shanta taught a student who has secured 80% marks and above on an average?"
 a = school_hash.select {|x| x[:teacher] == "Shanta"}
-b = a.map {|x| ((x[:mark1].to_i+x[:mark2].to_i)/2) >= 80} 
-if b.index(true)
-  puts true
-else
-  puts false
-end
+puts "#{a.any? {|x| ((x[:mark1].to_i+x[:mark2].to_i)/2) >= 80}}, Shanta_taught_with_distinction."
 puts "2) "
 print "i) Teachers names are: " 
-print school_hash.map { |x| x.values[0] }.uniq
+print school_hash.map { |x| x[:teacher] }.uniq
 puts ''
 print "ii) Students names are: "
-print school_hash.map { |x| x.values[1] }.uniq
+print school_hash.map { |x| x[:student] }.uniq
 puts ''
 puts "3) How many subjects do Murali study?"
 print "Number of subjects Murali study: "
-#a = school_hash.group_by{|x| x[:student]}.map{|x,y| [x, y.size]}.to_h
-#puts a["Murali"]
 puts school_hash.count { |x| x[:student] == 'Murali' }
 puts "4) Is there a girl in the class?"
-a = school_hash.map { |x| x[:gender] == 'female' }
-if a.index(true)
-  puts true
-else
-  puts false
-end
+puts a = school_hash.any? { |x| x[:gender] == 'female' }
 puts "5) Find all students who have secured above 85% in any subject?"
 a = school_hash.find_all {|x| ((x[:mark1].to_i+x[:mark2].to_i)/2) >= 85}
 print a.map {|x| x[:student]}.uniq
 puts " "
 puts "6) Have all students secured above 80% in Geography?"
 a = school_hash.select {|x| x[:subject] == "Geography"}
-b = a.map {|x| ((x[:mark1].to_i+x[:mark2].to_i)/2) >= 80} 
-if b.index(false)
-  puts false
-else
-  puts true
-end
+puts a.all? {|x| ((x[:mark1].to_i+x[:mark2].to_i)/2) >= 80} 
 puts "7) what is the maximum mark received by a student and what subject?"
-a = school_hash.max_by{|x| ((x[:mark1].to_i+x[:mark2].to_i)/2) } 
+a = school_hash.max_by {|x| ((x[:mark1].to_i+x[:mark2].to_i)/2) } 
 puts "#{a[:student]} scored the maximum mark in #{a[:subject]}"
 puts "8) what is the minimum mark in mathematics and by who?"
 a = school_hash.select {|x| x[:subject] == "Maths"}
 b = a.min_by{|x| ((x[:mark1].to_i+x[:mark2].to_i)/2)}
 puts "#{b[:student]} scored the minimum mark #{(b[:mark1].to_f+b[:mark2].to_f)/2} in #{b[:subject]}"
 puts "9) has anyone secured less than 50% marks?"
-a = school_hash.map{|x| ((x[:mark1].to_i+x[:mark2].to_i)/2) < 50 } 
-if a.index(false)
-  puts true
-else
-  puts false
-end
+puts a = school_hash.any?{|x| ((x[:mark1].to_i+x[:mark2].to_i)/2) < 50 } 
 puts "10) Find the person who got the highest mark in geography?"
 a = school_hash.select {|x| x[:subject] == "Geography"}
 b = a.max_by{|x| ((x[:mark1].to_i+x[:mark2].to_i)/2)}
 puts "#{b[:student]} got the hishest mark in geography"
 puts "11) Find the lowest mark secured by male students in the class?"
 a = school_hash.select {|x| x[:gender] == 'male'}
-b = a.min_by{|x| ((x[:mark1].to_i+x[:mark2].to_i)/2)} 
+b = a.min_by {|x| ((x[:mark1].to_i+x[:mark2].to_i)/2)} 
 puts "#{b[:student]} got the lowest mark in the class"
